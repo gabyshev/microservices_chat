4496f862193ca475f88b250d5eca1ecf24c42462
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-02-24 00:41:33 +0600

    sign_in user after successful token check

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -6,10 +6,19 @@ class ApplicationController < ActionController::Base
   protected
 
   def verify_jwt_token
-    head :unauthorized if auth_headers.nil? || !AuthToken.valid?(auth_headers.split(' ').last)
+    return head :unauthorized if auth_headers.nil? || !AuthToken.valid?(provided_token)
+    sign_in(:user, User.find_by(email: provided_email))
   end
 
   def auth_headers
     request.headers['Authorization']
+  end
+
+  def provided_token
+    auth_headers.split(' ')[-1]
+  end
+
+  def provided_email
+    auth_headers.split(' ')[-2]
   end
 end
