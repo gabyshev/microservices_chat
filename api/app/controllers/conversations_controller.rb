acd129fb46d92e43bed1b566f5df913121fcf406
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-02-24 06:35:36 +0600

    get conversation from api

--- api/app/controllers/conversations_controller.rb
+++ api/app/controllers/conversations_controller.rb
@@ -5,4 +5,16 @@ class ConversationsController < ApplicationController
     @users = User.where.not(id: current_user.id)
     respond_with @users
   end
+
+  def create
+    sender    = User.find(params[:sender_id])
+    recipient = User.find(params[:recipient_id])
+
+    @conversation = unless Conversation.between(sender.id, recipient.id).empty?
+      Conversation.between(sender.id, recipient.id).first
+    else
+      Conversation.create(sender: sender, recipient: recipient)
+    end
+    render json: @conversation.to_json
+  end
 end
