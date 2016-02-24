7cdb5d2d2d408f87eae1a2d297d8c2308d5ccee9
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-02-24 06:39:36 +0600

    relations + ability to find or create conversation between 2 users

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223180641) do
+ActiveRecord::Schema.define(version: 20160223224500) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 20160223180641) do
 
   add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id"
   add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id"
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                      null: false
